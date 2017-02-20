b5f15ebae3c548191ad8889450a8c689a24005ff
Chris Severns <severnsc@gmail.com>
2017-02-20 11:09:46 -0500

    Add RESTful actions with views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,23 +1,51 @@
 class KittensController < ApplicationController
 
 	def new
+		@kitten = Kitten.new
 	end
 
 	def create
+		@kitten = Kitten.new(kitten_attributes)
+		if @kitten.save
+			flash[:success] = "Kitten created!"
+			redirect_to kitten_path(@kitten)
+		else
+			render 'new'
+		end
 	end
 
 	def show
+		@kitten = Kitten.find(params[:id])
 	end
 
 	def index
+		@kittens = Kitten.all
 	end
 
 	def edit
+		@kitten = Kitten.find(params[:id])
 	end
 
 	def update
+		@kitten = Kitten.find(params[:id])
+		if @kitten.update_attributes(kitten_attributes)
+			flash[:success] = "Kitten updated!"
+			redirect_to kitten_path(@kitten)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@kitten = Kitten.find(params[:id])
+		@kitten.delete
+		flash[:success] = "Kitten deleted!"
+		redirect_to root_path
+	end
+
+	private
+
+	def kitten_attributes
+		params.require(:kitten).permit(:name, :age, :cuteness, :softness)
 	end
 end
